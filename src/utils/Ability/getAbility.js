293ca67be163280b8c524483a0e5f5ffc6d320b3
PazitivelyN0where <awesometom.broccoli@gmail.com>
2021-08-04 15:16:54 -0400

    Folder restructuring

--- src/utils/Ability/getAbility.js
+++ src/utils/Ability/getAbility.js
@@ -3,6 +3,11 @@ const  ABILITY_URL = 'https://pokeapi.co/api/v2/ability';
 
 
 //=======================$ability helper functions====================//
+async function getAbilityData(message) {
+    const ability = message.content.split(" ")[1];
+    const abData = await getAbility(ability);
+    return abData;
+}
 
 async function getAbility(ability){
     let response = await fetch(`${ABILITY_URL}/${ability}`);
@@ -10,4 +15,4 @@ async function getAbility(ability){
 }
 
 //=======================EXPORTS====================//
-module.exports= { getAbility };
+module.exports= { getAbility, getAbilityData };
