d707a29f4d4be2fce468225ef738b0a7ad958daf
manoonan1 <mnoonan136@gmail.com>
2021-08-12 20:11:34 -0400

    $shiny $abmon $stats parser correction

--- src/utils/Pokemon/GetPokemon.js
+++ src/utils/Pokemon/GetPokemon.js
@@ -1,10 +1,13 @@
 const fetch = require('node-fetch');
+const Fuse = require('fuse.js');
 var fs = require('fs');
 const  POKEMON_URL = 'https://pokeapi.co/api/v2/pokemon';
 
 //=======================$general helper functions====================//
 async function getPokemonJSON(pokemon) {
-    let response = await fetch(`${POKEMON_URL}/${pokemon}`);
+    correctPokemon = await correctPokemonInput(pokemon)
+    console.log(correctPokemon[0]);
+    response = await fetch(`${POKEMON_URL}/${correctPokemon[0]}`);
     const pokemonData = response.json();
     return pokemonData;
 }
@@ -24,5 +27,27 @@ function readJSON(path){
     return JSON.parse(data);   
 }
 
+async function correctPokemonInput(pokemon){
+    let response = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1118");
+    const pokemonNameList = await response.json();
+    const options = {
+        includeScore: true,
+        keys: ['name']
+    }
+    const fuse = new Fuse(pokemonNameList.results, options);
+    const result = fuse.search(pokemon);
+    let candidates = compileResultList(result);
+    return candidates;
+}
+
+function compileResultList(result){
+    let count = 0;
+    let pokemonResultList = [result[count].item.name];
+    while(result[count].score == result[count+1].score){
+        pokemonResultList.push(result[count+1].item.name);
+        count++;
+    }
+    return pokemonResultList;
+}
 //=======================EXPORTS====================//
 module.exports = { getPokemonJSON, isVariant, readJSON };
